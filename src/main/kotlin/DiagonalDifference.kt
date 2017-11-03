233f2a05fbb52a4ceb31096010c7e262b01ae4d1
Ke Xie <xie.kenneth@gmail.com>
2017-11-03 21:48:06 +0800

    Commit first kotlin

--- src/main/kotlin/DiagonalDifference.kt
+++ src/main/kotlin/DiagonalDifference.kt
@@ -4,11 +4,8 @@ import java.util.*
 fun main(args: Array<String>) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
     val N = readLine()!!.toInt()
-    //generateSequence(1) { it + 1 }.take(N).forEach { println(it) }
-    //(1..N).toList().forEach { println(it) }
     val matrix = (1..N).map { readLine()!!.split(" ").map { it.toInt() }}
 
-    val diff = Math.abs(matrix.withIndex().map { it.value[it.index] }.sum() - matrix.withIndex().map { it.value[N - 1 - it.index] }.sum())
-
+    val diff = Math.abs(matrix.withIndex().map { (i, v) -> v[i] }.sum() - matrix.withIndex().map { it.value[N - 1 - it.index] }.sum())
     println(diff)
 }
