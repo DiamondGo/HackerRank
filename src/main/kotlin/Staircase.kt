77cec11ca7e456fcf09b56601955bf1664c34d8f
MasterCoderK <8674856+DiamondGo@users.noreply.github.com>
2018-07-23 23:41:58 -0700

    Resolve 3 problems.

---
+++ src/main/kotlin/Staircase.kt
@@ -0,0 +1,35 @@
+// https://www.hackerrank.com/challenges/staircase/problem
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the staircase function below.
+fun staircase(n: Int): Unit {
+    (1..n).forEach {
+        println(" ".repeat(n - it) + "#".repeat(it))
+    }
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    staircase(n)
+}
